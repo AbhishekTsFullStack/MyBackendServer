8ca3de33e27cb26ad0677d9a5beed73cbabf6ae0
Abhishek sharma <99015589+SharmaG-Dev@users.noreply.github.com>
2023-06-28 11:24:22 +0530

    completed

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const experess = require("express")
 require("./connection")
 require('dotenv').config()
+const CustomerRouter = require("./Routers/AuthRouters/customerRouter")
 const app = experess()
 
 
+app.use(experess.json())
 
+
+app.use("/customer", CustomerRouter);
+app.use("/", (req, res) => {
+    res.send("Chal gaaya bhai")
+})
 
 
 
