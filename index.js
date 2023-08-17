9cbf8f32590b6d9e1e56a99163442a29d1d80b99
SharmaG-Dev <99015589+SharmaG-Dev@users.noreply.github.com>
2023-08-17 10:47:21 +0530

    Uploaded

--- index.js
+++ index.js
@@ -2,46 +2,46 @@ require('dotenv').config();
 require("./connection");
 const express = require("express");
 const cors = require("cors");
-const passport = require('passport')
-const cookieSession = require('cookie-session')
-const CustomerRouter = require('./Routers/AuthRouters/customerRouter')
-const ServiceRouter = require("./Routers/Services/ServicesRoutes")
-const utilRouter = require("./Routers/utils")
-const RolesRoutes = require("./Routers/RolesManageRoutes/RolesRoutes")
-const EmployeeRoutes = require("./Routers/AuthRouters/OfficeRouter")
-const ServiceRoutes = require("./Routers/AuthRouters/ServiceProviderRoutes")
-const OrderRouters = require('./Routers/OrdersRoutes/BookingRoutes')
-const EnquiryRouters = require('./Routers/enquiryRoutes')
-const ExpenseRouters = require('./Routers/ExpensesRouters/ExpenseRouters')
-const SuperAdminRoutes = require("./Routers/AuthRouters/SuperAdminRoutes")
-const app = express()
-// varibles
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const CustomerRouter = require('./Routers/AuthRouters/customerRouter');
+const ServiceRouter = require("./Routers/Services/ServicesRoutes");
+const utilRouter = require("./Routers/utils");
+const RolesRoutes = require("./Routers/RolesManageRoutes/RolesRoutes");
+const EmployeeRoutes = require("./Routers/AuthRouters/OfficeRouter");
+const ServiceRoutes = require("./Routers/AuthRouters/ServiceProviderRoutes");
+const OrderRouters = require('./Routers/OrdersRoutes/BookingRoutes');
+const EnquiryRouters = require('./Routers/enquiryRoutes');
+const ExpenseRouters = require('./Routers/ExpensesRouters/ExpenseRouters');
+const SuperAdminRoutes = require("./Routers/AuthRouters/SuperAdminRoutes");
+const app = express();
 
-
-// middlewares
-app.use(express.json())
+// Middleware section
+app.use(express.json());
 app.use(
     cookieSession({
         name: "session",
         keys: ["helpers"],
-        maxAge: 24 * 60 * 60 * 100, // 24 hours in milliseconds
+        maxAge: 24 * 60 * 60 * 1000, // Fixed: Correct the maxAge value to milliseconds
     })
-)
-// initialize the passport
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://trackel-helper-frontend.vercel.app"],
+        origin: ["http://ec2-13-53-74-96.eu-north-1.compute.amazonaws.com", "http://localhost:3000", "https://trackel-helper-frontend.vercel.app"],
         credentials: true,
     })
 );
-const port = process.env.PORT || 5000
+
+// Routes section
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Helper Services")
-})
+    res.send("Welcome to the Helper Services");
+});
+
 app.use("/customer", CustomerRouter);
 app.use("/service", ServiceRouter);
 app.use("/expense", ExpenseRouters);
@@ -52,15 +52,11 @@ app.use("/admin", SuperAdminRoutes);
 app.use("/util", utilRouter);
 app.use("/order", OrderRouters);
 app.use('/enquiry', EnquiryRouters);
-// app.use("/auth", googleAuth);
+
+// Static files
 app.use(express.static("./static"));
 
-
-
-
-
-
-// listen the port
+// Start the server
 app.listen(port, () => {
-    console.log(`server started at port ${port}`)
-})
+    console.log(`Server started at port ${port}`);
+});
