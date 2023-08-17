06bea06e0c73f27461748cf3bfa132b6d1001cd9
SharmaG-Dev <99015589+SharmaG-Dev@users.noreply.github.com>
2023-08-17 17:38:52 +0530

    Updated api

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ const app = express();
 
 // Middleware section
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://ec2-3-111-248-12.ap-south-1.compute.amazonaws.com'); // Replace with your allowed origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(
     cookieSession({
         name: "session",
